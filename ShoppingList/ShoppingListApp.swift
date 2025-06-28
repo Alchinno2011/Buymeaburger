466ebc06f2f35cb0026113a231c5f76fcf5319e9
Alchinno2011 <konstantinbegun1330@gmail.com>
2025-06-28 14:53:10 +0200

    Initial Commit

---
+++ ShoppingList/ShoppingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListApp.swift
+//  ShoppingList
+//
+//  Created by konstantin on 28/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
