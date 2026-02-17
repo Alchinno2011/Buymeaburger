d9e6fd38f5f6412f698b405ab3efea59d55512e5
Alchinno2011 <konstantinbegun1330@gmail.com>
2026-02-17 17:46:58 +0100

    Change comment

--- backend/shoppingList_backend/Controllers/AddItem.cs
+++ backend/shoppingList_backend/Controllers/AddItem.cs
@@ -13,7 +13,7 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace shoppingList_backend.Controllers
 {
-    //adds a new item to the grocery list and adds it to the recomendations if it is not already there
+    //adds a new item to the grocery list and adds it to the recomendations if it is not already there.
 
 
     [Route("newitem")]
